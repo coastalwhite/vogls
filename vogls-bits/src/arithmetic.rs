037f03c7d26af4e588c01a802deff91f0384819f
coastalwhite <me@gburghoorn.com>
2026-01-19 08:17:11 +0100

    fix fv bitwise kernels and add reduce kernels

--- vogls-bits/src/arithmetic.rs
+++ vogls-bits/src/arithmetic.rs
@@ -16,6 +16,8 @@ pub enum FvLogicValue {
 }
 
 impl FvLogicValue {
+    pub const VALUES: &[FvLogicValue] = &[Self::X, Self::L0, Self::L1, Self::Z];
+
     #[inline(always)]
     pub const fn from_bool(value: bool) -> Self {
         match value {
@@ -43,6 +45,7 @@ impl From<bool> for FvLogicValue {
 
 pub trait BitwisePart:
     Copy
+    + std::fmt::Debug
     + std::ops::BitAnd<Output = Self>
     + std::ops::BitOr<Output = Self>
     + std::ops::BitXor<Output = Self>
@@ -53,6 +56,8 @@ pub trait BitwisePart:
 {
     const NUM_BITS: u32;
     fn splat_byte(b: u8) -> Self;
+    fn mask(size: VectorSize) -> Self;
+    fn count_ones(self) -> u32;
 }
 
 macro_rules! impl_bitwise_part {
@@ -68,7 +73,15 @@ macro_rules! impl_bitwise_part {
                 }
                 Self::from_le_bytes(bs)
             }
-
+            #[inline(always)]
+            fn mask(size: VectorSize) -> Self {
+                let one: Self = 1;
+                one.unbounded_shl(size.get()).wrapping_sub(1)
+            }
+            #[inline(always)]
+            fn count_ones(self) -> u32 {
+                Self::count_ones(self)
+            }
         }
         )+
     };
@@ -80,6 +93,14 @@ impl BitwisePart for FvLogicValue {
     #[inline(always)]
     fn splat_byte(b: u8) -> Self {
         Self::from_repr(b & 0b11)
+    }
+    #[inline(always)]
+    fn mask(size: VectorSize) -> Self {
+        Self::from_repr(1u8.unbounded_shl(size.get()).wrapping_sub(1))
+    }
+    #[inline(always)]
+    fn count_ones(self) -> u32 {
+        (self as u8).count_ones()
     }
 }
 
@@ -148,7 +169,8 @@ const EVEN_BITS_BYTE: u8 = 0xAA;
 /// Does the `value` have a `Unknown` or `High Impedance` value?
 #[inline(always)]
 pub fn has_fv_non_logical<T: BitwisePart>(value: T) -> bool {
-    (value ^ (value >> 1)) != T::splat_byte(ODD_BITS_BYTE)
+    let odd_bits = T::splat_byte(ODD_BITS_BYTE);
+    (value ^ (value >> 1)) & odd_bits != odd_bits
 }
 
 #[inline(always)]
@@ -180,8 +202,8 @@ pub fn fv_bitwise_inv<T: BitwisePart>(value: T) -> T {
     // z  0               z  0
     let x0 = value & T::splat_byte(ODD_BITS_BYTE);
     let x1 = value & T::splat_byte(EVEN_BITS_BYTE);
-    let z0 = !x0 & x1;
-    let z1 = x0 & !x1;
+    let z0 = !x0 & (x1 >> 1);
+    let z1 = (x0 << 1) & !x1;
     z1 | z0
 }
 #[inline(always)]
@@ -194,7 +216,7 @@ pub fn fv_bitwise_and<T: BitwisePart>(x: T, y: T) -> T {
     //
     // z1z0 = fv.and(x1x0, y1y0)
     //
-    // z0 = x0 y1b + y0 x1b         z1 = x1 y1 x0b y0b
+    // z0 = x0 x1b + y0 y1b         z1 = x1 y1 x0b y0b
     //    x  0  1  z                   x  0  1  z
     // x  0  1  0  0                x  0  0  0  0
     // 0  1  1  1  1                0  0  0  0  0
@@ -204,7 +226,7 @@ pub fn fv_bitwise_and<T: BitwisePart>(x: T, y: T) -> T {
     let x1 = x & T::splat_byte(EVEN_BITS_BYTE);
     let y0 = y & T::splat_byte(ODD_BITS_BYTE);
     let y1 = y & T::splat_byte(EVEN_BITS_BYTE);
-    let z0 = (x0 & !(y1 >> 1)) | (y0 & !(x1 >> 1));
+    let z0 = (x0 & !(x1 >> 1)) | (y0 & !(y1 >> 1));
     let z1 = x1 & y1 & !(x0 << 1) & !(y0 << 1);
     z1 | z0
 }
@@ -218,7 +240,7 @@ pub fn fv_bitwise_or<T: BitwisePart>(x: T, y: T) -> T {
     //
     // z1z0 = fv.or(x1x0, y1y0)
     //
-    // z0 = x1b y1b x0 y0         z1 = x0b y1 + y0b x1
+    // z0 = x1b y1b x0 y0         z1 = x0b x1 + y0b y1
     //    x  0  1  z                   x  0  1  z
     // x  0  0  0  0                x  0  0  1  0
     // 0  0  1  0  0                0  0  0  1  0
@@ -229,15 +251,15 @@ pub fn fv_bitwise_or<T: BitwisePart>(x: T, y: T) -> T {
     let y0 = y & T::splat_byte(ODD_BITS_BYTE);
     let y1 = y & T::splat_byte(EVEN_BITS_BYTE);
     let z0 = !(x1 >> 1) & !(y1 >> 1) & x0 & y0;
-    let z1 = (!(x0 << 1) & y1) | (!(y0 << 1) & x1);
+    let z1 = (!(x0 << 1) & x1) | (!(y0 << 1) & y1);
     z1 | z0
 }
 #[inline(always)]
 pub fn fv_bitwise_xor<T: BitwisePart>(x: T, y: T) -> T {
     // &  x  0  1  z
     // x  x  x  x  x
-    // 0  x  1  0  x
-    // 1  x  0  1  x
+    // 0  x  0  1  x
+    // 1  x  1  0  x
     // z  x  x  x  x
     //
     // z1z0 = fv.xor(x1x0, y1y0)
@@ -248,6 +270,14 @@ pub fn fv_bitwise_xor<T: BitwisePart>(x: T, y: T) -> T {
     // 0  0  1  0  0                   0  0  0  1  0
     // 1  0  0  1  0                   1  0  1  0  0
     // z  0  0  0  0                   z  0  0  0  0
+    //
+    // Components:
+    // x1^x0:              y1^y0:              x0^y0:
+    //    x  0  1  z          x  0  1  z          x  0  1  z
+    // x  0  1  1  0       x  0  0  0  0       x  0  1  0  1
+    // 0  0  1  1  0       0  1  1  1  1       0  1  0  1  0
+    // 1  0  1  1  0       1  1  1  1  1       1  0  1  0  1
+    // z  0  1  1  0       z  0  0  0  0       z  1  0  1  0
     let x0 = x & T::splat_byte(ODD_BITS_BYTE);
     let x1 = x & T::splat_byte(EVEN_BITS_BYTE);
     let y0 = y & T::splat_byte(ODD_BITS_BYTE);
@@ -271,6 +301,73 @@ pub fn fv_equality<T: BitwisePart>(x: T, y: T) -> FvLogicValue {
     }
 
     FvLogicValue::from_bool(x == y)
+}
+#[inline(always)]
+pub fn fv_reduce_and<T: BitwisePart>(x: T, size: VectorSize) -> FvLogicValue {
+    // &  x  0  1  z
+    // x  x  0  x  x
+    // 0  0  0  0  0
+    // 1  x  0  1  x
+    // z  x  0  x  x
+    let zero = T::splat_byte(0);
+    let mask = T::mask(VectorSize::new(size.get() * 2).unwrap());
+    let odd_bits = T::splat_byte(ODD_BITS_BYTE);
+    let even_bits = T::splat_byte(EVEN_BITS_BYTE);
+
+    let x0 = x & odd_bits;
+    let x1 = x & even_bits;
+
+    let z0 = (!(x1 >> 1) & x0) != zero;
+    let z1 = (x1 == even_bits & mask) & (x0 == zero);
+
+    FvLogicValue::from_repr((u8::from(z1) << 1) | u8::from(z0))
+}
+#[inline(always)]
+pub fn fv_reduce_or<T: BitwisePart>(x: T, size: VectorSize) -> FvLogicValue {
+    // &  x  0  1  z
+    // x  x  x  1  x
+    // 0  x  0  1  x
+    // 1  1  1  1  1
+    // z  x  x  1  x
+    let zero = T::splat_byte(0);
+    let mask = T::mask(VectorSize::new(size.get() * 2).unwrap());
+    let odd_bits = T::splat_byte(ODD_BITS_BYTE);
+    let even_bits = T::splat_byte(EVEN_BITS_BYTE);
+
+    let x0 = x & odd_bits;
+    let x1 = x & even_bits;
+
+    let z0 = (x1 == zero) & (x0 == odd_bits & mask);
+    let z1 = x1 & !(x0 << 1) != zero;
+
+    FvLogicValue::from_repr((u8::from(z1) << 1) | u8::from(z0))
+}
+#[inline(always)]
+pub fn fv_reduce_xor<T: BitwisePart>(x: T, size: VectorSize) -> FvLogicValue {
+    // &  x  0  1  z
+    // x  x  x  x  x
+    // 0  x  0  1  x
+    // 1  x  1  0  x
+    // z  x  x  x  x
+    let odd_bits = T::splat_byte(ODD_BITS_BYTE);
+    let even_bits = T::splat_byte(EVEN_BITS_BYTE);
+
+    let x0 = x & odd_bits;
+    let x1 = x & even_bits;
+
+    let t0 = !(x1 >> 1) & x0;
+    let t1 = x1 & !(x0 << 1);
+
+    let num_l0 = t0.count_ones();
+    let num_l1 = t1.count_ones();
+
+    let t2 = num_l1 % 2 == 0;
+    let t3 = num_l0 + num_l1 == size.get();
+
+    let z0 = t2 & t3;
+    let z1 = !t2 & t3;
+
+    FvLogicValue::from_repr((u8::from(z1) << 1) | u8::from(z0))
 }
 
 pub fn tv_addition(dst: &mut [u8], lhs: &[u8], rhs: &[u8], size: VectorSize) {
@@ -323,4 +420,109 @@ pub fn tv_modulus(dst: &mut [u8], lhs: &[u8], rhs: &[u8], size: VectorSize) {
     let r = load_partial_u64(&rhs, size);
     let out = l.wrapping_rem(r);
     store_partial_u64(dst, out, size);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use FvLogicValue as L;
+
+    #[rustfmt::skip]
+    const FV_BITWISE_AND_LUT: [L; 16] = [
+        // x       0     1      z      &
+        L::X,  L::L0, L::X,  L::X,  // x
+        L::L0, L::L0, L::L0, L::L0, // 0
+        L::X,  L::L0, L::L1, L::X,  // 1
+        L::X,  L::L0, L::X,  L::X,  // z
+    ];
+    #[rustfmt::skip]
+    const FV_BITWISE_OR_LUT: [L; 16] = [
+        // x       0     1      z      &
+        L::X,  L::X,  L::L1, L::X,  // x
+        L::X,  L::L0, L::L1, L::X,  // 0
+        L::L1, L::L1, L::L1, L::L1, // 1
+        L::X,  L::X,  L::L1, L::X,  // z
+    ];
+    #[rustfmt::skip]
+    const FV_BITWISE_XOR_LUT: [L; 16] = [
+        // x       0     1      z      &
+        L::X,  L::X,  L::X,  L::X,  // x
+        L::X,  L::L0, L::L1, L::X,  // 0
+        L::X,  L::L1, L::L0, L::X,  // 1
+        L::X,  L::X,  L::X,  L::X,  // z
+    ];
+    #[rustfmt::skip]
+    const FV_BITWISE_INV_LUT: [L; 4] = [
+        // x       0      1      z
+        L::X,  L::L1, L::L0,  L::X,
+    ];
+    #[rustfmt::skip]
+    const FV_EQUALITY_LUT: [L; 16] = [
+        // x       0     1      z      &
+        L::X,  L::X,  L::X,  L::X,  // x
+        L::X,  L::L1, L::L0, L::X,  // 0
+        L::X,  L::L0, L::L1, L::X,  // 1
+        L::X,  L::X,  L::X,  L::X,  // z
+    ];
+
+    #[test]
+    fn test_fv_bitwise() {
+        for &y in L::VALUES {
+            for &x in L::VALUES {
+                let z_and = fv_bitwise_and(x, y);
+                let z_or = fv_bitwise_or(x, y);
+                let z_xor = fv_bitwise_xor(x, y);
+
+                let concat = ((x as u8) << 2) | (y as u8);
+                let size = VectorSize::new(2).unwrap();
+                let z_redand = fv_reduce_and(concat, size);
+                let z_redor = fv_reduce_or(concat, size);
+                let z_redxor = fv_reduce_xor(concat, size);
+
+                let idx = (((y as u8) << 2) | (x as u8)) as usize;
+                assert_eq!(z_and, FV_BITWISE_AND_LUT[idx], "{z_and:?} != {x:?} & {y:?}");
+                assert_eq!(z_or, FV_BITWISE_OR_LUT[idx], "{z_or:?} != {x:?} | {y:?}");
+                assert_eq!(z_xor, FV_BITWISE_XOR_LUT[idx], "{z_xor:?} != {x:?} ^ {y:?}");
+
+                assert_eq!(
+                    z_redand, FV_BITWISE_AND_LUT[idx],
+                    "{z_redand:?} != &{{ {x:?}, {y:?} }}"
+                );
+                assert_eq!(
+                    z_redor, FV_BITWISE_OR_LUT[idx],
+                    "{z_redor:?} != | {{ {x:?}, {y:?} }}"
+                );
+                assert_eq!(
+                    z_redxor, FV_BITWISE_XOR_LUT[idx],
+                    "{z_redxor:?} != ^ {{ {x:?}, {y:?} }}"
+                );
+            }
+        }
+    }
+    #[test]
+    fn test_fv_equality() {
+        for &y in L::VALUES {
+            for &x in L::VALUES {
+                let z = fv_equality(x, y);
+
+                let idx = (((y as u8) << 2) | (x as u8)) as usize;
+                assert_eq!(z, FV_EQUALITY_LUT[idx], "{z:?} != ({x:?} == {y:?})");
+            }
+        }
+    }
+    #[test]
+    fn test_fv_bitwise_inv() {
+        for &x in L::VALUES {
+            let z = fv_bitwise_inv(x);
+            let idx = (x as u8) as usize;
+            assert_eq!(z, FV_BITWISE_INV_LUT[idx], "{z:?} = !{x:?}");
+        }
+    }
+    #[test]
+    fn test_fv_has_non_logical() {
+        assert!(has_fv_non_logical(L::X));
+        assert!(!has_fv_non_logical(L::L0));
+        assert!(!has_fv_non_logical(L::L1));
+        assert!(has_fv_non_logical(L::Z));
+    }
 }
