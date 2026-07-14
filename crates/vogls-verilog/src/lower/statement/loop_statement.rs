60589a2cb4225a85e79d89e9d9ecdfbec4dfd4b3
coastalwhite <me@gburghoorn.com>
2026-07-14 17:48:19 +0200

    Update repeat to use signal instead

--- crates/vogls-verilog/src/lower/statement/loop_statement.rs
+++ crates/vogls-verilog/src/lower/statement/loop_statement.rs
@@ -1,5 +1,8 @@
 use vogls_frontend::symbol_table::SymbolId;
-use vogls_ir::{BasicBlockBuilder, BasicBlockTerminator};
+use vogls_ir::token_range::TokenRange;
+use vogls_ir::{
+    BasicBlockBuilder, BasicBlockTerminator, Bits, INTEGER_VSIZE, LogicMode, Signal, SignalFlags,
+};
 
 use crate::ast::statement::{LoopStatement, LoopStatementVariant};
 use crate::ast::{AstId, AstIdRange};
@@ -17,12 +20,19 @@ pub fn lower_loop_statement<'a>(
 
     let ls = &*ls;
 
-    let mut repeat_vars = None;
+    let mut repeat = None;
     match ls.variant {
         V::Repeat(size) => {
             let (size, _) = lower_expr(ctx, mctx, scope, &mut builder, size, None)?;
-            let i = builder.constant_u32(mctx.gl(), 0);
-            repeat_vars = Some((i, size));
+            let signal = mctx.gl.signals.insert(Signal {
+                name: String::from("__REPEAT_VAR"),
+                size: INTEGER_VSIZE,
+                initialize: Some(Bits::new_u32(0)),
+                mode: LogicMode::TwoValue,
+                flags: SignalFlags::EMPTY,
+                origin: TokenRange::default(),
+            });
+            repeat = Some((signal, size));
         }
         V::For(initialization, _, _) => {
             let initialization = &*initialization;
@@ -50,19 +60,15 @@ pub fn lower_loop_statement<'a>(
         V::Forever | V::While(_) => {}
     }
 
-    let predecessor = builder.key();
     builder = builder.jump(mctx.gl());
 
     let loop_start = builder.key();
-    let mut repeat_i_phi = None;
     let condition = match ls.variant {
         V::Forever => None,
         V::Repeat(_) => {
-            let (i, size) = repeat_vars.as_mut().unwrap();
-            let phi_ref;
-            (*i, phi_ref) = builder.phi(mctx.gl(), [(predecessor, *i), (predecessor, *i)].into());
-            repeat_i_phi = Some(phi_ref);
-            Some(builder.unsigned_lt(mctx.gl(), *i, *size))
+            let (signal, size) = repeat.unwrap();
+            let v = builder.probe(mctx.gl(), signal);
+            Some(builder.unsigned_lt(mctx.gl(), v, size))
         }
         V::While(condition) => Some(lower_expr(ctx, mctx, scope, &mut builder, condition, None)?.0),
         V::For(_, condition, _) => {
@@ -106,13 +112,10 @@ pub fn lower_loop_statement<'a>(
             )?;
         }
         V::Repeat(_) => {
-            let (i, _) = repeat_vars.unwrap();
-
-            let one = builder.constant_u32(mctx.gl(), 1);
-            let i_plus_1 = builder.plus(mctx.gl(), i, one);
-
-            let phi_ref = repeat_i_phi.unwrap();
-            builder.update_phi_ref(mctx.gl(), phi_ref, 1, builder.key(), i_plus_1);
+            let (signal, _) = repeat.unwrap();
+            let v = builder.probe(mctx.gl(), signal);
+            let v = builder.plus_constant(mctx.gl(), v, Bits::new_u32(1));
+            builder.drive(mctx.gl(), signal, v);
         }
         V::Forever | V::While(_) => {}
     }
